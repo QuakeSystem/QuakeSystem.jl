90879988edc218d48287445d5ad7c9ff0fb9e532
Alexander Koelzer <a.j.koelzer@uu.nl>
2025-11-28 00:05:35 +0100

    updated plotting, noticed error but did not fix in this file

--- snippets/RSF/0D_hooke.jl
+++ snippets/RSF/0D_hooke.jl
@@ -1,4 +1,5 @@
 # assume constant stressing rate
+# THIS IMPLEMENTATION IS WRONG BUT IT SOMEHOW WORKS REALLY WELL
 using Plots
 include("calcyield_rsf.jl")
 include("calcvpvisc.jl")
@@ -12,7 +13,7 @@ dy = 500
 # material parameters
 a = 0.011 # rate parameter
 b = 0.015 # state paramter
-L = 0.01 # characteristic slip distance [m]
+L = 0.05 # characteristic slip distance [m]
 mu0 = 0.5 # reference friction
 V0 = 4e-9 # reference slip rate [m/s]
 lambda = 0 # fluid pressure ratio
@@ -38,8 +39,7 @@ state = 0
 # initial viscoplastic viscosity
 etavp = etav
 # initial stress
-tau = 0
-
+tau = 0e6
 function timestep(oldstate, Vp, etavp, tau)
     vx = Vp / 2 # update vx for dt calculation (assume all slip is on x-axis)
     dt = calc_dt(P, lambda, G, etavp, L, V0, poisson, dx, dy, vx, vy, Vp, oldstate)
@@ -50,13 +50,16 @@ function timestep(oldstate, Vp, etavp, tau)
     # NAVIER STOKES WOULD BE SOLVED HERE
     Vp = calc_sliprate_RSF(a, b, mu0, V0, newstate, P, tau, lambda, cohesion)
     # reduce stress according to slip
+    # this would need to be multiplied by dt to make sense, but that doesn't work while this does
     taudiff = -2 * G * Vp / (2 * dx)
+    #println(taudiff)
     tau = tau + taudiff
     return dt, newstate, mud, etavp, Vp, tau
 end
 
 # time step count
 niter = 1500
+#niter = 80000
 
 # initialize arrays
 Vps = zeros(niter)
@@ -81,12 +84,16 @@ times = cumsum(dts)
 istrength = P .* (mu0 .+ b * states)
 
 #plot(times, states)
-p = plot(times[500:end] / 3600 / 24 / 365.25, Vps[500:end], yscale=:log10)
+p = plot(times[1:end] / 3600 / 24 / 365.25, Vps[1:end], yscale=:log10)
 xlabel!("Time in years")
 ylabel!("Vp in m/s")
 display(p)
-p = plot(times[500:end] / 3600 / 24 / 365.25, taus[500:end] / 1e6, label="stress")
-#p = plot!(times[500:end]  / 3600 / 24 / 365.25, istrength[500:end]/1e6,label="interface strength")
+p = plot(times[1:end] / 3600 / 24 / 365.25, taus[1:end] / 1e6, label="stress")
+p = plot!(times[1:end] / 3600 / 24 / 365.25, istrength[1:end] / 1e6, label="interface strength")
 xlabel!("Time in years")
 ylabel!("stress in MPa")
+display(p)
+p = plot(dts, yscale=:log10)
+xlabel!("Time step no.")
+ylabel!("dt in s")
 display(p)
