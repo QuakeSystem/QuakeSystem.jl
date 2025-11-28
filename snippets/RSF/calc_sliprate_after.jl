a6a78ce0467dc992f13a975b4c0779e98e821535
Alexander Koelzer <a.j.koelzer@uu.nl>
2025-11-28 18:38:41 +0100

    added experimental functions for quasi-dynamic RSF (untested)

--- snippets/RSF/calc_sliprate_after.jl
+++ snippets/RSF/calc_sliprate_after.jl
@@ -1,9 +1,21 @@
 # recalculate plastic slip rate after solving N-S
-function calc_sliprate_RSF(a, b, mu0, V0, state, P, tauii, lambda=0, cohesion=0)
-    if tauii > 0
-        Vp_after = 2 * V0 * sinh(max((tauii - cohesion), 0) / (a * P * (1 - lambda))) * exp(-(mu0 + b * state) / a)
-    else
-        Vp_after = 0
+begin
+    function calc_sliprate_RSF(a, b, mu0, V0, state, P, tauii, lambda=0, cohesion=0)
+        if tauii > 0
+            Vp_after = 2 * V0 * sinh(max((tauii - cohesion), 0) / (a * P * (1 - lambda))) * exp(-(mu0 + b * state) / a)
+        else
+            Vp_after = 0
+        end
+        return Vp_after
     end
-    return Vp_after
+    function calc_sliprate_RSF_qd(a, b, mu0, V0, state, P, tauii, G, rho, lambda=0, cohesion=0)
+        cs = sqrt(G / rho) # shear wave velocity
+        etaqd = G / (2 * cs) # radiation damping term
+        if tauii > 0
+            Vp_after = 2 * V0 * sinh(max((tauii - cohesion - etaqd), 0) / (a * P * (1 - lambda))) * exp(-(mu0 + b * state) / a)
+        else
+            Vp_after = 0
+        end
+        return Vp_after
+    end
 end
--- snippets/RSF/calcyield_rsf.jl
+++ snippets/RSF/calcyield_rsf.jl
@@ -1,11 +1,25 @@
 # calculate new (dimensionless) state, dynamic friction coefficient, and yield strength
-function calcyield(a, b, L, mu0, V0, Vp, dt, oldstate, P, lambda=0, cohesion=0)
-    if (Vp * dt / L <= 1e-6)
-        newstate = log(exp(oldstate) * (1 - Vp * dt / L) + V0 * dt / L)
-    else
-        newstate = log(V0 / Vp + (exp(oldstate) - V0 / Vp) * exp(-Vp * dt / L))
+begin
+    function calcyield(a, b, L, mu0, V0, Vp, dt, oldstate, P, lambda=0, cohesion=0)
+        if (Vp * dt / L <= 1e-6)
+            newstate = log(exp(oldstate) * (1 - Vp * dt / L) + V0 * dt / L)
+        else
+            newstate = log(V0 / Vp + (exp(oldstate) - V0 / Vp) * exp(-Vp * dt / L))
+        end
+        mud = a * asinh(Vp / (2 * V0) * exp((mu0 + b * newstate) / a))
+        syield = P * (1 - lambda) * mud + cohesion
+        return newstate, mud, syield
     end
-    mud = a * asinh(Vp / (2 * V0) * exp((mu0 + b * newstate) / a))
-    syield = P * (1 - lambda) * mud + cohesion
-    return newstate, mud, syield
+    function calcyield_qd(a, b, L, mu0, V0, Vp, dt, oldstate, P, G, rho, lambda=0, cohesion=0)
+        cs = sqrt(G / rho) # shear wave velocity
+        etaqd = G / (2 * cs) # radiation damping term
+        if (Vp * dt / L <= 1e-6)
+            newstate = log(exp(oldstate) * (1 - Vp * dt / L) + V0 * dt / L)
+        else
+            newstate = log(V0 / Vp + (exp(oldstate) - V0 / Vp) * exp(-Vp * dt / L))
+        end
+        mud = a * asinh(Vp / (2 * V0) * exp((mu0 + b * newstate) / a))
+        syield = P * (1 - lambda) * mud + cohesion + etaqd
+        return newstate, mud, syield
+    end
 end
